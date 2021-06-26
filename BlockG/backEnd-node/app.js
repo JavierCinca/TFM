61a4938d5c6212d1b688d5aff1103b2daa566e60
JavierCinca <javier.cinca.perez@alumnos.upm.es>
2021-06-26 11:32:18 +0200

    BlockG

---
+++ BlockG/backEnd-node/app.js
@@ -0,0 +1,93 @@
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  methodOverride = require("method-override");
+
+var cors = require('cors');
+app.use(cors());
+
+const Web3 = require("web3");
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+const QRContractABI = [{"inputs":[{"internalType":"string","name":"_nombre","type":"string"},{"internalType":"string","name":"_email","type":"string"}],"name":"compraEntrada","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"string","name":"_nombre","type":"string"},{"internalType":"uint256","name":"_fecha","type":"uint256"},{"internalType":"uint256","name":"_cost","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"stateMutability":"payable","type":"receive"},{"inputs":[],"name":"admin","outputs":[{"internalType":"addresspayable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"asistente","outputs":[{"internalType":"string","name":"nombre","type":"string"},{"internalType":"string","name":"email","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"asistentes","outputs":[{"internalType":"addresspayable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"asistentesLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"cost","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"fecha","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"nombre","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"whoAmI","outputs":[{"internalType":"string","name":"_nombre","type":"string"},{"internalType":"string","name":"_email","type":"string"}],"stateMutability":"view","type":"function"}];
+const QRContract = new web3.eth.Contract(QRContractABI, '0x1BDC897526AfDf2C4Bb0cA2823Ed08184C6B3196'); //Change in first execution
+let platformMainAddr = '0xb4aF85e0Dc6Ad65a1E3B68f527B6ca4b1146B51a'; //Change in first execution
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+var router = express.Router();
+
+/**
+ * Solicita una peticion de compra 
+ * 
+ * Request Body
+ * {
+ * "address": "0x5C3c837B9d1E6C5B18E98F51e8E5aa265b37f0DC",
+ * "password": "!@superpassword",
+ * "eventName": "Champions League",
+ * "email": "test66@gmail.com",
+ * "price": 230
+ * }
+ */
+router.post("/requestTicket", async function (req, res) {
+    //Unlock account
+    console.table(req.body);
+    web3.eth.personal.unlockAccount(req.body.address, req.body.password, 15000);
+    //Send transaction
+    var returnedValue = await QRContract.methods.compraEntrada(req.body.eventName, req.body.email).send({ from: req.body.address, value: req.body.price, gas:3000000});
+
+    if (returnedValue.status === true) {
+      res.send(JSON.stringify('GREEN'));
+
+    } else {
+      res.send(JSON.stringify('YELLOW'));
+    }
+  });
+
+  /**
+   * Genera una nueva cuenta que se le anade 5 ether
+   *  
+   * RequestBody
+   * {
+   *  "password" : "!@superpassword"
+   * }
+   */
+  router.post("/requestNewAccount", async function (req, res) {
+    (async () => {
+      let newAccount = await web3.eth.personal.newAccount(req.body.password);
+      web3.eth.sendTransaction({from: platformMainAddr, to:newAccount, value: web3.utils.toWei('5', 'ether'), gasLimit: 21000, gasPrice: 20000000000});
+
+      const response = {"address" : newAccount};
+      res.status(200).send(JSON.stringify(response));
+    })();
+  });
+
+
+  /**
+   * Obtener el precio del evento
+   */
+router.get("/getEventPrice", async function (req, res) {
+  var returnedValue = await QRContract.methods.cost().call();
+
+    res.status(200).send(JSON.stringify(returnedValue));
+});
+
+  /**
+   * Obtener el precio del evento
+   * 
+   * URL Path
+   * /getAddressEthBalance/0xBDe64338b109F5C144aa66a484f068fF094f2072
+   */
+   router.get("/getAddressEthBalance/:address", async function (req, res) {
+    let returnedValue = await web3.eth.getBalance(req.params.address);
+    let convertedValueFromWei = web3.utils.fromWei(returnedValue , 'ether')
+  
+    res.status(200).send(JSON.stringify(convertedValueFromWei));
+  });
+
+app.use(router);
+
+app.listen(3000, function () {
+  console.log("Node server running on http://localhost:3000");
+});
---
+++ BlockG/mobile_QR_app/src/app/home/home.page.ts
@@ -0,0 +1,206 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { ToastController, LoadingController, Platform } from '@ionic/angular';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import jsQR from 'jsqr';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss']
+})
+export class HomePage {
+  @ViewChild('video', { static: false }) video: ElementRef;
+  @ViewChild('canvas', { static: false }) canvas: ElementRef;
+  @ViewChild('fileinput', { static: false }) fileinput: ElementRef;
+
+  canvasElement: any;
+  videoElement: any;
+  canvasContext: any;
+  scanActive = false;
+  scanResult = null;
+  user_address = null;
+  address = null;
+  password = '!@superpassword';
+  email = 'tmpMail@hgmail.com';
+  loading: HTMLIonLoadingElement = null;
+  url: any="http://localhost:3000/";
+  eventPrice: any;
+
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json'});
+  options = { headers: this.headers };
+  responseForToast: any;
+
+  constructor(
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private plt: Platform,
+    private http: HttpClient,
+    public toastController: ToastController
+  ) {
+    const isInStandaloneMode = () =>
+      'standalone' in window.navigator && window.navigator['standalone'];
+    if (this.plt.is('ios') && isInStandaloneMode()) {
+      console.log('I am a an iOS PWA!');
+      // E.g. hide the scan functionality!
+    }
+  }
+
+  ngAfterViewInit() {
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasContext = this.canvasElement.getContext('2d');
+    this.videoElement = this.video.nativeElement;
+    this.requestNewAccount();
+    this.getEventPrice();
+  }
+
+  async presentToast(response) {
+    let toast;
+    console.log(response)
+    if(response=="GREEN"){
+    toast = await this.toastController.create({
+      message: 'Registro completado con ÉXITO',
+      duration: 2000,
+      color:"success"
+    });
+    }else if(response=="YELLOW"){
+    toast = await this.toastController.create({
+      message: 'AVISO!! Ya estas registrado al evento',
+      duration: 2000,
+      color:"warning"
+    });
+    }else{
+    toast = await this.toastController.create({
+      message: 'ERROR!! Fallo en el servicio',
+      duration: 2000,
+      color:"danger"
+    });
+    }
+    toast.present();
+  }
+
+  reset() {
+    this.scanResult = null;
+  }
+
+  stopScan() {
+    this.scanActive = false;
+  }
+
+  async startScan() {
+    // Not working on iOS standalone mode!
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment' }
+    });
+
+    this.videoElement.srcObject = stream;
+    // Required for Safari
+    this.videoElement.setAttribute('playsinline', true);
+
+    this.loading = await this.loadingCtrl.create({});
+    await this.loading.present();
+
+    this.videoElement.play();
+    requestAnimationFrame(this.scan.bind(this));
+  }
+
+  async scan() {
+    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
+      if (this.loading) {
+        await this.loading.dismiss();
+        this.loading = null;
+        this.scanActive = true;
+      }
+
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
+
+      this.canvasContext.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+
+      if (code) {
+        this.scanActive = false;
+        this.scanResult = code.data;
+      } else {
+        if (this.scanActive) {
+          requestAnimationFrame(this.scan.bind(this));
+        }
+      }
+    } else {
+      requestAnimationFrame(this.scan.bind(this));
+    }
+  }
+
+  captureImage() {
+    this.fileinput.nativeElement.click();
+  }
+
+  handleFile(files: FileList) {
+    const file = files.item(0);
+
+    var img = new Image();
+    img.onload = () => {
+      this.canvasContext.drawImage(img, 0, 0, this.canvasElement.width, this.canvasElement.height);
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+
+      if (code) {
+        this.scanResult = code.data;
+      }
+    };
+    img.src = URL.createObjectURL(file);
+  }
+
+  sendCode(){
+  let route='requestTicket';
+  this.scanResult=JSON.parse(this.scanResult);
+  this.scanResult.address=this.address;
+  this.scanResult.price=this.eventPrice;
+  this.scanResult.password=this.password;
+  this.scanResult.email=this.email;
+  this.scanResult=JSON.stringify(this.scanResult);
+
+  this.http.post(this.url+route, this.scanResult, this.options).subscribe(data => {
+    this.responseForToast=data;
+    this.presentToast(this.responseForToast);
+  });
+  }
+
+  requestNewAccount(){
+    let json_pass= {
+      "password" : this.password
+      }
+    let route='requestNewAccount';
+    this.http.post(this.url+route,json_pass, this.options).subscribe(data => {
+     this.address=data["address"];
+    });
+  }
+
+  getEventPrice(){
+    let route='getEventPrice';
+    this.http.get(this.url+route, this.options).subscribe(data => {
+      this.eventPrice=data;
+     });
+  }
+}
